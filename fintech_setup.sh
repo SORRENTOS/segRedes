6fac55b41112ecacbf9223a99c024166d16fe06e
SORRENTOS <106037109+SORRENTOS@users.noreply.github.com>
2026-03-28 16:45:42 -0300

    paso2/5

--- fintech_setup.sh
+++ fintech_setup.sh
@@ -5,3 +5,7 @@ echo "[*] 0/5: Inicio de aprovisionamiento (modo no interactivo APT)."
 echo "[*] 1/5: Actualizando repositorios y sistema..."
 # Ahora ejecútalo directamente (esta parte sí corre cambios reales)
 sudo apt-get update -qq && sudo apt-get upgrade -y -qq
+echo "[*] 2/5: Instalando SOC (Suricata, Docker, Python3)..."
+
+# Instala ahora
+sudo apt-get install -y -qq python3-pip python3-venv suricata docker.io docker-compose curl git jq software-properties-common
